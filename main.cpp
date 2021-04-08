48d599d1f46a26dec6e6be1d0c4743471b724fa2
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-04-08 17:44:01 +0530

    Add functions

--- main.cpp
+++ main.cpp
@@ -1,12 +1,47 @@
+#include <stdlib.h>
+
 #include <iostream>
 
 using namespace std;
 
+void search_by_name_or_symbol() {}
+
+void search_by_atomic_number() {}
+
+void display_periodic_table() {}
+
 int main(int argc, char const *argv[]) {
-    cout << "1. Search element by name or sysmbol";
-    cout << "2. Search element by atomic number";
-    cout << "3. Display periodic table";
-    cout << "4. Exit";
+    int choice;
+
+    while (true) {
+        cout << "\t1. Search element by name or sysmbol" << endl;
+        cout << "\t2. Search element by atomic number" << endl;
+        cout << "\t3. Display periodic table" << endl;
+        cout << "\t4. Exit" << endl;
+        cout << "Enter your choice : " << endl;
+        cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                search_by_name_or_symbol();
+                break;
+            }
+            case 2: {
+                search_by_atomic_number();
+                break;
+            }
+            case 3: {
+                display_periodic_table();
+                break;
+            }
+            case 4: {
+                exit(0);
+            }
+            default: {
+                break;
+            }
+        }
+    }
 
     return 0;
 }
